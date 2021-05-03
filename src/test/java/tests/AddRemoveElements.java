dfa61b9d8be30bf58f9ae053f1bed4f728153c79
Olga Kudina <olga.kudzina@gmail.com>
2021-05-03 17:33:20 +0300

    New tests - AddRemoveElements and Dropdown

---
+++ src/test/java/tests/AddRemoveElements.java
@@ -0,0 +1,34 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+@Test
+public class AddRemoveElements {
+    public void addRemoteElements() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.get("http://the-internet.herokuapp.com/add_remove_elements/");
+        WebElement addButton = driver.findElement(By.xpath("//*[@id=\"content\"]/div/button"));
+        addButton.click();
+        addButton.click();
+
+        driver.findElement(By.xpath("//*[@id=\"elements\"]/button[2]")).click();
+        boolean isDeleteButtonDisplayed = driver.findElement(By.xpath("//*[@id=\"elements\"]/button"));
+        Assert.assertTrue(isDeleteButtonDisplayed);
+
+
+
+
+
+
+    }
+}
